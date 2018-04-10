313080fbd08cec6bdfea870f98aa22f448749125
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2018-04-10 09:56:54 +0300

    L%

---
+++ DanceProj.dpr
@@ -0,0 +1,14 @@
+program DanceProj;
+
+uses
+  Vcl.Forms,
+  Dance in 'Dance.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
