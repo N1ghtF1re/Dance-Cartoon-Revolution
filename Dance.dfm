d86588ed57ce7242f382e8a39085653bd79d49b5
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2018-04-10 10:20:14 +0300

    Keks

--- Dance.dfm
+++ Dance.dfm
@@ -11,17 +11,16 @@ object Form1: TForm1
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
-  object Image1: TImage
-    Left = 0
-    Top = 0
-    Width = 696
-    Height = 385
-    Align = alClient
-    ExplicitLeft = 112
-    ExplicitTop = 48
-    ExplicitWidth = 105
-    ExplicitHeight = 105
+  object Button1: TButton
+    Left = 152
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
   end
 end
--- Dance.pas
+++ Dance.pas
@@ -4,22 +4,36 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
-    Image1: TImage;
+    Button1: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
   end;
 
+
 var
   Form1: TForm1;
-
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Canvas.MoveTo(0,0);
+  canvas.LineTo(99,99);
+  //Form1.Paint;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Form1.Paint;
+end;
 
 end.
