5a1afbb3f8ff6025c31018b00f126754acd92c94
Alex <sasha.pankratiew@gmail.com>
2018-05-14 10:42:36 +0300

    Revolution

--- Dance.dfm
+++ Dance.dfm
@@ -2,9 +2,10 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 320
-  ClientWidth = 523
+  ClientHeight = 462
+  ClientWidth = 688
   Color = clBtnFace
+  DoubleBuffered = True
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
@@ -15,22 +16,40 @@ object Form1: TForm1
   PixelsPerInch = 96
   TextHeight = 13
   object mp1: TMediaPlayer
-    Left = 0
-    Top = 355
+    Left = 8
+    Top = 451
     Width = 253
     Height = 30
     AutoOpen = True
     DeviceType = dtWaveAudio
     DoubleBuffered = True
-    FileName = 'E:\Dance-Cartoon-Revolution\mus\kek.wav'
+    FileName = '..\..\mus\18-Baauer-HarlemShake.wav'
     Visible = False
     ParentDoubleBuffered = False
     TabOrder = 0
   end
-  object Timer1: TTimer
-    Interval = 506
-    OnTimer = Timer1Timer
+  object Timer276: TTimer
+    Interval = 276
+    OnTimer = Timer276Timer
     Left = 256
-    Top = 352
+    Top = 272
+  end
+  object TimerMain: TTimer
+    Interval = 80
+    OnTimer = TimerMainTimer
+    Left = 328
+    Top = 216
+  end
+  object timer138: TTimer
+    Interval = 138
+    OnTimer = timer138Timer
+    Left = 368
+    Top = 144
+  end
+  object TimerStartDance: TTimer
+    Interval = 13450
+    OnTimer = TimerStartDanceTimer
+    Left = 528
+    Top = 208
   end
 end
--- Dance.pas
+++ Dance.pas
@@ -9,7 +9,7 @@ uses
 
 Const
   MainX = 200;
-  MainY = 200;
+  MainY = 300;
   k = 60;
   coefHead = 0.25 * k;
   coefBody = 1 * k;
@@ -27,25 +27,41 @@ type
   x,y: integer;
   end;
   TForm1 = class(TForm)
-      Timer1: TTimer;
+    Timer276: TTimer;
     mp1: TMediaPlayer;
+    TimerMain: TTimer;
+    timer138: TTimer;
+    TimerStartDance: TTimer;
       procedure FormCreate(Sender: TObject);
       procedure Button1Click(Sender: TObject);
-      procedure Timer1Timer(Sender: TObject);
+      procedure Timer276Timer(Sender: TObject);
+    procedure TimerMainTimer(Sender: TObject);
+    procedure timer138Timer(Sender: TObject);
+    procedure TimerStartDanceTimer(Sender: TObject);
     private
       { Private declarations }
     public
       { Public declarations }
     end;
-    TQueueArr = Array[1..N] of integer;
+    TQueueArr = Array[0..N-1] of integer;
 const
   Queue:TQueueArr = (1,2,0,1,2,1,0);
-var
+  Colors: array[0..6] of TColor = (clRed, clMaroon, clGreen, clAqua, clBlue, clFuchsia, clPurple);
+
+  var
   DanceMode: TMode;
   Form1: TForm1;
   state:Integer;
   kek: Boolean;
+  lol: integer;
+  isMem: boolean;
+  isStartDance: Boolean;
+  col: TColor;
+  mem: Integer;
   pic: TBitmap;
+  j: integer;
+  currState: Integer;
+  lr1, lr2: TLR;
 //  KeyPoints: array [0..10] of TPoint;
 implementation
 
@@ -68,6 +84,7 @@ procedure paintHead(Canvas:TCanvas; Coord: TPoint);
 var R: integer;
 begin
   R:= Round(coefHead);
+  canvas.Brush.Color := clBlack;
   Canvas.Ellipse(Coord.x - R, Coord.y, Coord.x + R, Coord.y - 2 * R);
 end;
 
@@ -195,12 +212,12 @@ begin
 end;
 
 // Coord - координаты места, откуда растут руки (из жопы)
-procedure WritePeople(Canvas:TCanvas; Coord: TPoint; mode:TMode);
+procedure WritePeople(Canvas:TCanvas; Coord: TPoint; mode:TMode; LR1, LR2: TLR);
 var
   InitArm, InitLegs, initHead, initBody: TPoint;
-  LR1, LR2: TLR;
 begin
   //ShowMessage( IntToStr(Coord.x) + ' ' + IntToSTR(Coord.y));
+  canvas.Pen.Width := 3;
   InitArm := Coord;
   initHead := Coord;
   InitLegs := Coord;
@@ -224,61 +241,153 @@ begin
   // Находим точку начала тела
   PaintHead(Canvas, InitHead);
   PaintBody(Canvas, InitBody);
-  if kek then
-  begin
-    LR1 := left;
-    LR2 := right;
-    kek := false
-  end
-  else
-  begin
-    LR1 := right;
-    LR2 := left;
-    kek := true;
-  end;
 
   PaintArm(Canvas, InitArm, mode, LR1);
   PaintLegs(Canvas, InitLegs, mode, LR2);
+  canvas.Pen.Width := 1;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+  isStartDance := false;
   Form1.Paint;
-   state:=1;
+   state:=0;
   mp1.Play;
+  mem := 800;
+  lol := 800;
 end;
 
-procedure clearscreen(Form: TForm1; Canvas: TCanvas);
+procedure clearscreen(Form: TForm1; Canvas: TCanvas; color: TColor);
+var
+oldcolor: TColor;
 begin
+  oldcolor:= Canvas.Brush.Color;
+  canvas.Brush.Color := color;
   Canvas.Rectangle(0,0, Form.Width, form.Height);
+  canvas.Brush.Color := oldcolor;
 end;
 
-procedure TForm1.Timer1Timer(Sender: TObject);
+
+procedure drawDiscoBall(Canvas: TCanvas; const x,y,r: integer; color: TColor);
+var
+oldcolor: TColor;
+begin
+  oldcolor:= Canvas.Brush.Color;
+  canvas.MoveTo(x, y-r);
+  canvas.LineTo(x, 0);
+  canvas.Brush.Color := color;
+  canvas.Ellipse(x-r,y-r,x+r,y+r);
+  canvas.Brush.Color := oldcolor;
+end;
+
+procedure drawSporsmen(canvas: TCanvas; x,y: INTEGER);
+var
+P:TPoint;
+begin
+  p.x := x;
+  p.y := y;
+  WritePeople(canvas,p, MovLegs, LEft, Right );
+end;
+
+
+procedure TForm1.Timer276Timer(Sender: TObject);
 var
   P: TPoint;
-
+  i: integer;
 begin
-  clearscreen(form1, canvas);
+  inc (state);
+  state := state mod 7;
 
+  if kek then
+  begin
+    LR1 := Tlr(0);
+    LR2 := TLR(1);
+    kek := false
+  end
+  else
+  begin
+    LR1 := TLR(1);
+    LR2 := TLR(0);
+    kek := true;
+  end;
+
+  // 1 2 3 4 5 6 7 8
+
+end;
+
+procedure TForm1.TimerMainTimer(Sender: TObject);
+var
+  P: TPoint;
+  i: integer;
+begin
+
+  clearscreen(form1, canvas,col);
   canvas.Draw(0,0, pic);
   P.x := MainX;
   P.y := MainY;
   Canvas.Pen.Width:= 2;
   //WritePeople(Canvas, P, MovArms);
   //canvas.Pen.Color:= clred;
-  WritePeople(Canvas, P, TMode( Queue[state] ) );
+  WritePeople(Canvas, P, TMode( Queue[state] ) , lr1, lr2);
   canvas.Pen.Color := clblack;
-  p.x := p.x + 190;
-  //WritePeople(Canvas, P, TMode(Random(3)));
-  WritePeople(Canvas, P, TMode( Queue[state] ));
-  p.x := p.x + 220;
-  //WritePeople(Canvas, P, TMode(Random(3)));
-  WritePeople(Canvas, P, TMode( Queue[state] ));
 
-  if state >= 7 then
-    state := 1
-  else
-    inc(state);
+
+
+  currstate := state;
+  for i := 1 to 15 do
+  begin
+    p.x := p.x + k*2;
+    if i mod 6 = 0 then
+    begin
+      p.x := p.x - k*2*i;
+      P.y := p.y + 3*k;
+    end;
+
+    if not isStartDance then
+    begin
+      WritePeople(Canvas, P, TMode( MovArms ), lr1, lr2);
+    end
+    else
+    begin
+      inc (currstate, 5);
+      currstate := currstate mod 7;
+
+      WritePeople(Canvas, P, TMode( Queue[currstate] ), lr1, lr2);
+    end;
+
+  end;
+
+  drawDiscoBall(canvas,Form1.Width div 2 ,k + 40,trunc(k*0.6),random(1000000));
+
+  mem := mem - 10;
+  if mem  mod 100 = 0 then
+  begin
+    isMem:= True;
+    j:= 4;
+  end;
+
+  if isMem then
+  begin
+    if j <= 2 then
+      lol := lol + 40
+    else
+      lol := lol - 40;
+    if j = 1 then
+      isMem := false;
+    dec(j);
+  end;
+
+  drawSporsmen(Canvas,mem, lol);
+end;
+
+procedure TForm1.TimerStartDanceTimer(Sender: TObject);
+begin
+  isStartDance := true;
+end;
+
+procedure TForm1.timer138Timer(Sender: TObject);
+begin
+  col := Colors[random(7)];
 end;
 
 end.
